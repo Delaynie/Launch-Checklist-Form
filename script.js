f78d1106ee25f25b12f3e87d82bf50b2cfbd0890
Delaynie <apple@Delaynies-MacBook-Pro.local>
2020-12-14 15:42:27 -0600

    working on shuttle requirement updates

--- script.js
+++ script.js
@@ -5,40 +5,32 @@ window.addEventListener("load", function() {
    let copilotName = document.querySelector("input[name=copilotName]");
    let fuelLevel = document.querySelector("input[name=fuelLevel]");
    let cargoMass = document.querySelector("input[name=cargoMass]");
-   let faultyItems = document.getElementById("faultyItems");
    let launchStatus = document.getElementById("launchStatus");
+   let pilotStatus = document.getElementById("pilotStatus");
+   let copilotStatus = document.getElementById("copilotStatus");
+   let faultyItems = document.getElementById("faultyItems");
    
    formSubmit.addEventListener("click", (event) => {
-
-      if (pilotName.value == "" || copilotName.value == "") {
+// validation for form entries
+      if (pilotName.value == "" || copilotName.value == "" || fuelLevel.value == "" || cargoMass.value == "") {
          window.alert("you must provide an answer for each field!");
          event.preventDefault();
       }
+      // validation to make sure data type is correct
       if (isNaN(Number(fuelLevel.value)) == true || isNaN(Number(cargoMass.value)) == true) {
-        window.alert("you must provide correct data.");
+        window.alert("you must provide a number for the fuel level and cargo mass.");
         event.preventDefault();
      }
-
+// Shuttle Requirement Updates
+   pilotStatus.innerHTML = `${pilotName.value} is ready`;
+   copilotStatus.innerHTMl = `${copilotName.value} is ready`;
+     if (fuelLevel.value < 10000) {
+        //change faultyItems list to visible
+        fuelLevel.innerHTML = `There is not enough fuel for the journey`;
+        launchStatus.innerHTML = `Shuttle not ready for Launch.`
+     }
    });
 })
-
-/*
-
-id="pilotStatus"
-id="copilotStatus"
-id="fuelStatus"
-id="cargoStatus"
-
-   let li = document.getElementById("pilotStatus");
-   li.addEventListener("submit", function(){
-      document.getElementById("pilotStatus").innerHTML = `${pilotName.getAttribute("pilotName")} is ready`;
-   })
-
-   let copolitName = this.document.getElementById("copilotName");
-   copolitName.addEventListener("submit", function(){
-      copolitStatus.innerHTML = `Copilot ${copilotName} boomReady!`;
-})*/
-
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
